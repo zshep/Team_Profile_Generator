da47983b62d537c251c72525442fbfc631c90bb9
zsheperdigian <zsheperdigian@gmail.com>
2022-09-09 17:25:55 -0700

    node gitIgnore

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,14 +1,13 @@
 const Employee = require("../lib/Employee");
+const joe = new Employee("joe", 01, "joe@yhoo.com");
 
     // Unit test - Employee
     describe("Employee", () =>{
         describe("name", () => {
 
             it("should generate name from Employee class constructor", () => {
-                const username = `${}git `
-
-                
-                expect(result).toEqual(answer);
+               
+                expect(joe.name).toEqual("joe");
             })
 
 
