faca460b00d6d9d1b4d132fcde69e0ad3a4c91e0
zsheperdigian <zsheperdigian@gmail.com>
2022-09-11 20:57:44 -0700

    broke up the array of questions into 4 parts, manager questions (intial questions), menu questions, engineer quetsions, and intern questions. I got program to continually ask for enginer/intern if the user so desires

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 //load in needed libraries
-const inquirer  = require("inquirer");
+const inquirer = require("inquirer");
 const { Employee } = require("./lib/Employee");
-const { Manager  } = require("./lib/Manager");
+const { Manager } = require("./lib/Manager");
 const { Engineer } = require("./lib/Engineer");
 const { Intern } = require("./lib/Intern");
 const fs = require('fs');
 
 
 
+//array of questions for the manager
+const main_questions = [
 
-
-
-
-
-
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-const questions = [
-    
     {
         type: "input",
         message: "hello manager, what is your name?",
@@ -38,95 +31,154 @@ const questions = [
         message: "What is your office number?",
         name: "manager_office",
     },
+]
+// question for main menu (after manager has entered info)
+const continue_questions = [
     {
         type: "list",
         message: "How would you like to continue?",
         name: "menu",
         choices: ["I would like to add an Engineer", "I would like to add an intern", "I am finished"],
     },
+]
+// array of questions for the Engineer
+const engineer_questions = [
     {
-        // WHEN I select the engineer option
-        // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
         type: "input",
         message: "What is the name of your engineer?",
-        name: "engineer_name",
-        when: (answers) => answers.menu === "I would like to add an Engineer",
+        name: "engineer_name"
     },
     {
         type: "input",
         message: "What is the id of your engineer?",
-        name: "engineer_id",      
+        name: "engineer_id",
     },
     {
         type: "input",
         message: "What is the email of your engineer?",
-        name: "engineer_email",      
+        name: "engineer_email",
     },
     {
         type: "input",
         message: "What is the github username of your engineer?",
-        name: "engineer_github",      
+        name: "engineer_github",
     },
-    {
-        type: "list",
-        message: "How would you like to continue?",
-        name: "menu",
-        choices: ["I would like to add an Engineer", "I would like to add an intern", "I am finished"],   
-    },
-    // WHEN I select the intern option
-    // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+
+]
+
+//array of questions for the intern
+const intern_questions = [
     {
         type: "input",
         message: "What is the name of your intern?",
-        name: "intern_name",
-        when: (answers) => answers.menu === "I would like to add an intern",
+        name: "intern_name"
     },
+   
     {
         type: "input",
         message: "What is the id of your intern?",
-        name: "intern_id",      
+        name: "intern_id",
     },
     {
         type: "input",
         message: "What is the email of your intern?",
-        name: "intern_email",      
+        name: "intern_email",
     },
     {
         type: "input",
         message: "What school is your intern attending?",
-        name: "intern_school",      
+        name: "intern_school",
     },
-
-
-    
 ]
+
+
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
 //function to take data from inquirer and write the html
-function WriteHtml(answer){
-    
-    
+function WriteHtml(answer) {
+
+
 };
 
 
 
-
-
-
 //function to initialize the app
-function init() {
-    inquirer.prompt(questions)
+const init = async () => {
+    inquirer.prompt(main_questions)
 
         .then(answers => {
 
-        //debugging
-        console.table(answers);
-        //staring the writeHTML function
-        WriteHtml(answers);
+            //debugging
+            console.table(answers);
+
+            init_menu();
 
         })
 
 }
+
+//function for menu questions
+const init_menu = () => {
+    inquirer.prompt(continue_questions)
+
+        .then(answers => {
+
+            //debugging
+            console.table(answers);
+            switch(answers.menu) {
+                case "I would like to add an Engineer":    
+                    return init_engineer();
+
+                case "I would like to add an intern":
+                    return init_intern();
+
+                case "I am finished":
+                    console.log("the user has ended the app")
+                    return;
+            }
+           
+
+        })
+
+}
+
+
+//function for engineer questions
+const init_engineer = () => {
+    inquirer.prompt(engineer_questions)
+
+        .then(answers => {
+
+            //debugging
+            console.table(answers);
+
+            init_menu();
+
+        })
+
+}
+
+//function for intern questions
+
+const init_intern = () => {
+    inquirer.prompt(intern_questions)
+
+    .then(answers => {
+
+        //debugging
+        console.table(answers);
+
+        init_menu();
+
+    })
+
+}
+
+
+ //staring the writeHTML function
+//WriteHtml(answers);
+
+
 
 // GIVEN a command-line application that accepts user input
 //start the program by using:
